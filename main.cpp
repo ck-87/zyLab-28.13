266f5eaba3a470985510395e7d8f774ddd33d84e
ck-87 <150931732+ck-87@users.noreply.github.com>
2024-04-22 11:11:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
-#include <iostream> 
-using namespace std; 
-// update from user-1 
-//update from user-2
-int main(){
+#include "ItemToPurchase.h"
 
-return 0; 
-} 
+// Default constructor
+ItemToPurchase::ItemToPurchase()
+    : itemName("none"), itemPrice(0), itemQuantity(0) {}
 
+// Mutator and accessor functions
+void ItemToPurchase::SetName(const std::string& name) {
+    itemName = name;
+}
+
+std::string ItemToPurchase::GetName() const {
+    return itemName;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
+}
+
+int ItemToPurchase::GetPrice() const {
+    return itemPrice;
+}
+
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
+}
+
+int ItemToPurchase::GetQuantity() const {
+    return itemQuantity;
+}
 
 
 
